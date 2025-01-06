dd82bc7477c8a193657f38812e5ba01ea485cdbb
pcgsonae <pcgsonae@sonae.pt>
2025-01-06 12:20:53 +0000

    Update app.py

--- app.py
+++ app.py
@@ -917,7 +917,7 @@ if selected == 'Retail Sales Turnover':
   col_list = ['Date', 'geo', 'indic_bt']
   columns = np.append(col_list,prod)
   df = pd.DataFrame(retail_selection, columns=columns)
-  df[prod] = df[prod].apply(pd.to_numeric, errors='coerce')
+  #df[prod] = df[prod].apply(pd.to_numeric, errors='coerce')
   #df[columns] = df.round(decimals=1)
   #
   # Plot
